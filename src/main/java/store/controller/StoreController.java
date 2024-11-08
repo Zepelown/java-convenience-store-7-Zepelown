adf7da36eb42f4a6a45966e2adfaa17ed90be02f
Zepelown <dbswjdqhd12@gmail.com>
2024-11-08 12:28:03 +0900

    feat : Add products to buy input feature

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -1,20 +1,23 @@
 package store.controller;
 
-import store.data.entity.Product;
 import store.dto.ProductDto;
 import store.service.StoreDataService;
+import store.view.StoreInputView;
 import store.view.StoreOutputView;
 
 import java.io.IOException;
 import java.util.List;
 
 public class StoreController {
+    private final StoreInputView storeInputView = new StoreInputView();
     private final StoreOutputView storeOutputView = new StoreOutputView();
     private final StoreDataService storeDataService = new StoreDataService();
     public void start(){
         storeOutputView.printProductStockNotification();
         List<ProductDto> products = loadProductStock();
         storeOutputView.printProductStock(products);
+
+        String productsToBuy = storeInputView.getProductsToBuy();
     }
     private List<ProductDto> loadProductStock() {
         try {
