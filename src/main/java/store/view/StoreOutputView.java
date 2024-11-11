543c2be725a654b9d18319506991f35572a157b3
Zepelown <dbswjdqhd12@gmail.com>
2024-11-11 14:12:49 +0900

    fix: Fix error message prefix correctly

--- src/main/java/store/view/StoreOutputView.java
+++ src/main/java/store/view/StoreOutputView.java
@@ -18,7 +18,7 @@ public class StoreOutputView {
     private final DecimalFormat receiptDecimalFormat = new DecimalFormat("#,##0");
     private final String PRODUCT_STOCK_NOTIFICATION = "안녕하세요. W편의점입니다.\n" +
             "현재 보유하고 있는 상품입니다.";
-    private final String ERROR_MESSAGE_PREFIX = "[ERROR]";
+    private final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
 
     public void printProductStockNotification() {
         System.out.println(PRODUCT_STOCK_NOTIFICATION);
